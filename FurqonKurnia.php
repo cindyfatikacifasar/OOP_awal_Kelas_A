dbd3bec4b81506aecd00dfec5b2426c77f8791a1
furqonkurniaa <146910425+furqonkurniaa@users.noreply.github.com>
2023-11-21 14:56:00 +0700

    FurqonKurnia.php

--- FurqonKurnia.php
+++ FurqonKurnia.php
@@ -1,47 +1,3 @@
-<?php
-class mahasiswa
-{
-    // Properties
-    private $name;
-    private $nim;
-    private $matkul;
-    // Methods
-    function set_name($name)
-    {
-        $this->name = $name;
-    }
-    function get_name()
-    {
-        return $this->name;
-    }
-    function set_nim($nim)
-    {
-        $this->nim = $nim;
-    }
-    function get_nim()
-    {
-        return $this->nim;
-    }
-    function set_matkul($matkul)
-    {
-        $this->matkul = $matkul;
-    }
-    function get_matkul()
-    {
-        return $this->matkul;
-    }
-}
-
-$Furrz = new mahasiswa();
-$Furrz->set_name('Muhammad Ronaldo');
-$Furrz->set_nim('2255201018');
-$Furrz->set_matkul('pbo');
-echo "Name: " . $Furrz->get_name();
-echo "\n";
-echo "nim: " . $Furrz->get_nim();
-echo "\n";
-echo "matkul: " . $Furrz->get_matkul();
-
 <!DOCTYPE html>
 <html>
 <body>
@@ -83,13 +39,8 @@ class mahasiswa {
   function get_cita() {
   	return $this->cita;
   }
-}
-
-$Furqon = new mahasiswa();
-$Furqon->set_name('Furqon Kurnia');
-$Furqon->set_nim('2255201018');
-$Furqon->set_matkul('pbo');
-echo "Nama: " . $Furqon->get_name();
+  }
+      
 $Furqon = new mahasiswa('Furqon Kurnia', '2255201018', 'pbo', 'penjahat');
 echo $Furqon->get_name();
 echo "<br>";
