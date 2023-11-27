d31ffa2020a6be1dae9d41ddca975aad28c5a824
FauzanMRA <148047475+FauzanMRA@users.noreply.github.com>
2023-11-27 20:09:32 +0700

    update_fauzan.php
    Miftahul Fauzan
    2255201023
    protected

--- fauzan.php
+++ fauzan.php
@@ -24,22 +24,39 @@ class mahasiswa
         return $this->nim;
     }
 
-    function get_mk()
+    protected function get_mk()
     {
         return $this->mk;
     }
-    function get_citacita()
+    protected function get_citacita()
     {
         return $this->citacita;
     }
+    function __destruct()
+    {
+        echo "\nini akhir dari class mahasiswa uhuy {$this->name}.";
+    }
 }
 
-$fauzan = new mahasiswa('fzn', '2255201023', 'PBO', 'menjadi power ranger putih');
+//class turunan organisasi
+class organisasi extends mahasiswa
+{
+    function uhuy()
+    {
+        echo "Name: " . $this->get_name();
+        echo "\n";
+        echo "Nim: " . $this->get_nim();
+        echo "\n";
+        echo "Matkul: " . $this->get_mk();
+        echo "\n";
+        echo "Cita-cita: " . $this->get_citacita();
+        echo "\n";
+        echo "Organisasi : power ranger community";
+    }
 
-echo "Name: " . $fauzan->get_name();
-echo "\n";
-echo "nim: " . $fauzan->get_nim();
-echo "\n";
-echo "mk: " . $fauzan->get_mk();
-echo "\n";
-echo "citacita: " . $fauzan->get_citacita();
+}
+
+$fauzan = new organisasi('fzn', '2255201023', 'PBO', 'menjadi power ranger putih');
+$fauzan->uhuy();
+
+?>
