feded9ceb740b1db7dddc9082ee373ec6245ef0a
zikrialhadi <149349887+zikrialhadi@users.noreply.github.com>
2023-11-27 21:16:41 +0700

    Zikri_Alhadi(2255201008).php

--- Zikri_Alhadi(2255201008).php
+++ Zikri_Alhadi(2255201008).php
@@ -1,46 +1,60 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
-class mahasiswa {
+class Mahasiswa {
   // Properties
   private $name;
   private $nim;
   private $matkul;
   private $cita_cita;
-
+  
   // Methods
-  function __construct ($name, $nim, $matkul, $cita_cita){
-  	$this->name = $name;
+  function __construct($name, $nim, $matkul, $cita_cita) {
+    $this->name = $name;
     $this->nim = $nim;
     $this->matkul = $matkul;
     $this->cita_cita = $cita_cita;
   }
+  
   function get_name() {
     return $this->name;
   }
   function get_nim() {
     return $this->nim;
   }
-  function get_matkul() {
-  	return $this->matkul;
+  
+  protected function get_matkul() {
+    return $this->matkul;
   }
-  function get_cita_cita() {
-  	return $this->cita_cita;
+
+  protected function get_cita_cita() {
+    return $this->cita_cita;
+  }
+
+  function __destruct() {
+    echo "<br>ini akhir dari class mahasiswa {$this->name}";
   }
 }
 
-$zikri = new mahasiswa('zikri alhadi adha', '2255201008', 'pbo', 'Tentara');
-echo $zikri->get_name();
-echo "<br>";
-echo $zikri->get_nim();
-echo "<br>";
-echo $zikri->get_matkul();
-echo "<br>";
-echo $zikri->get_cita_cita();
+class Organisasi extends Mahasiswa {
+  public function name_organisasi() {
+    echo $this->get_name();
+    echo "<br>";
+    echo $this->get_nim();
+    echo "<br>";
+    echo $this->get_matkul();
+    echo "<br>";
+    echo $this->get_cita_cita();
+    echo "<br>";
+    echo "Gardu";
+    
+  }
+}
 
+$zikri = new Organisasi('Zikri Alhadi Adha', '2255201008', 'PBO', 'Tentara');
+$zikri->name_organisasi();
 ?>
- 
+
 </body>
 </html>
