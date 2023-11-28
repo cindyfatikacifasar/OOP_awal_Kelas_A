81fd98a183447f92e808e8538c54d87f8b76f0f4
furqonkurniaa <146910425+furqonkurniaa@users.noreply.github.com>
2023-11-28 21:59:10 +0700

    FurqonKurnia.php

--- FurqonKurnia.php
+++ FurqonKurnia.php
@@ -36,44 +36,45 @@ class mahasiswa
     echo "<br>Ini akhir dari class mahasiswa {$this->name}";
   }
 }
+//Class Turunan Organisasi
     class organisasi extends mahasiswa
     {
-    protected $Organisasi;
-    protected $TahunGabung;
+    protected $organisasi;
+    protected $tahungabung;
     function hima()
     {
-     echo "Nama: " . $Furrz->get_name();
+     echo "Nama: " . $this->get_name();
      echo "<br>";
-     echo "nim: " .  $Furrz->get_nim();
+     echo "nim: " .  $this->get_nim();
      echo "<br>";
-     echo "matkul: " . $Furrz->get_matkul();
+     echo "matkul: " . $this->get_matkul();
      echo "<br>";
-     echo "Cita-Cita: " . $Furrz->get_cita();
+     echo "Cita-Cita: " . $this->get_cita();
      echo "<br>";
      echo "Organisasi: " . $this->get_organisasi();
      echo "<br>";
      echo "Tahun Gabung: " . $this->get_tahungabung;
     }
-    function __construct($nama, $nim, $matkul, $cita, $organisasi, $tahungabung) {
-  parent::__construct($nama, $nim, $matkul, $cita) ;
+    function __construct($name, $nim, $matkul, $cita, $organisasi, $tahungabung) {
+  parent::__construct($name, $nim, $matkul, $cita) ;
   $this->organisasi = $organisasi;
   $this->tahungabung = $tahungabung;
   }
 protected function get_organisasi() {
   return $this->organisasi;
   }
-protected function get_tahun() {
-  return $this->gahungabung;
+protected function get_tahungabung() {
+  return $this->tahungabung;
   }
 function namatahun() {
-  echo "\n";
+  echo "<br>";
   echo "Organisasi: " . $this->get_organisasi();
-  echo "\n";
-  echo "Tahun Gabung: " . $this->get_tahun();
+  echo "<br>";
+  echo "Tahun Gabung: " . $this->get_tahungabung();
   }
 }       
-$Furrz = new mahasiswa('Furqon Kurnia', '2255201018', 'pbo', 'Hack Bank Israel', 'Pemuda Pancasila', '2022');
-$Furrz->Hima();   
+$Furrz = new organisasi('Furqon Kurnia', '2255201018', 'pbo', 'Hack Bank Israel', 'Pemuda Pancasila', '2022');
+$Furrz->hima();   
 
 ?>
 
