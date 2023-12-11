94e74f84faada24defd930c42698ca6a21db4a11
cindyfatikacifasar <149891202+cindyfatikacifasar@users.noreply.github.com>
2023-12-11 08:31:57 +0700

    CindyFatika2255201003.php
    Nama:CindyFatikaSari
    Nim:2255201003

--- CindyFatika2255201003.php
+++ CindyFatika2255201003.php
@@ -43,6 +43,27 @@ class Mahasiswa {
 
 //class turunan organisasi
 class organisasi extends mahasiswa {
+  //properties
+    protected $organisasi;
+    protected $tahun;
+
+  // methods
+    function __construct($name, $nim, $mata_kuliah, $citacita, $organisasi, $tahun)
+    {
+        parent::__construct($name, $nim, $mata_kuliah, $citacita);
+        $this->organisasi = $organisasi;
+        $this->tahun = $tahun;
+    }
+    
+    protected function get_organisasi()
+    {
+        return $this->organisasi;
+    }
+    protected function get_tahun()
+    {
+        return $this->tahun;
+    }
+  
       function bem() {
           echo "Name: " . $this->get_name();
           echo "<br>";
@@ -52,12 +73,14 @@ class organisasi extends mahasiswa {
           echo "<br>";
           echo "Cita-Cita: " . $this->get_citacita();
           echo "<br>";
-          echo "Organisasi : BEM"; 
+          echo "Nama organisasi: " . $this->get_organisasi();
+          echo "<br>";
+          echo "Tahun gabung: " . $this->get_tahun();
     }
  
 }
 
-$cindy = new organisasi('CindyFatika','2255201003','PBO','Pilotkereta');
+$cindy = new organisasi('CindyFatika','2255201003','PBO','Pilotkereta','BEM','2023');
 $cindy->bem();
 
 
